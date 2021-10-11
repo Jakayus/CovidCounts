97a841d410ec4e818e829dda7ef82d84c99fbf78
Jakayus <kawika@mail.usf.edu>
2021-10-10 23:00:18 -0700

    Added State Codable struct based upon API
    State model based upon API from diseas.sh, specificaly for US States

---
+++ CovidCountsApp/CovidCountsApp/Models/StateData.swift
@@ -0,0 +1,24 @@
+//
+//  StateData.swift
+//  CovidCountsApp
+//
+//  Created by Joel Sereno on 10/10/21.
+//
+
+// API documentation located here:
+// https://disease.sh/docs/#/COVID-19%3A%20Worldometers/get_v3_covid_19_states
+// This model reflects /v3/covid-19/states API
+
+
+import Foundation
+
+struct StateData: Codable, Identifiable {
+    
+    var id: String {
+        return state
+    }
+    
+    let state: String
+    let cases: Int
+    
+}
